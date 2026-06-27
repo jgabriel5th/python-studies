1bc99bc9374b795f583f0ffcfcf85891c6a9de3e
jgabriel5th <joaojklgabriel@gmail.com>
2026-06-27 16:32:50 -0300

    Another part of the exercise done

--- python_course/basic/lesson32_exercise.py
+++ python_course/basic/lesson32_exercise.py
@@ -19,4 +19,20 @@ Make a program that asks the time to the user and based by the time described,
 print the appropriated greeting. Ex.: Good morning 0-11, Good afternoon 12-17 and
 Good evening 18-23. <- Brazil time zone
 '''
+try:
+    time = input('Type the current time(24-hour format): ')
+    time = int(time)
+    good_morning = time <= 11
+    good_afternoon = time == 12 or time <= 17
+    good_evening = time == 18 or time <= 23
 
+    if good_morning:
+        print('Good morning!')
+    elif good_afternoon:
+        print('Good afternoon!')
+    elif good_evening:
+        print('Good evening!')
+    else:
+        print('You typed an invalid value')
+except:
+    print('Invalid value, type again')
