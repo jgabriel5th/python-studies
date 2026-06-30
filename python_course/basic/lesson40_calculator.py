094c990ecec86475c68142f8f54d8116bdaf936a
jgabriel5th <joaojklgabriel@gmail.com>
2026-06-30 14:24:00 -0300

    An upgrade of the calculator

--- python_course/basic/lesson40_calculator.py
+++ python_course/basic/lesson40_calculator.py
@@ -1,40 +1,48 @@
 '''Calculator with while-structure'''
-result = 0
 while True:
-    number1 = input('Type a number: ')
-    number2 = input('Type another number: ')
-    operator = input('Choose the math operation(sum/sub/multi/division): ')
-    if operator == 'sum':
-        number1 = int(number1)
-        number2 = int(number2)
-        result = number1 + number2
-        print(f'The result of your sum is {result}')
+    try:
+        number1 = input('Type a number: ')
+        number2 = input('Type another number: ')
+        operator = input('Choose the math operation(sum/sub/multi/division): ').lower()
+        sumSelection = operator == 'sum'
+        subSelection = operator == 'sub'
+        multiSelection = operator == 'multi'
+        divisionSelection = operator == 'division'
 
-    if operator == 'sub':
-        number1 = int(number1)
-        number2 = int(number2)
-        result = number1 - number2
-        print(f'The result of your subtraction is {result}')
+        if sumSelection:
+            number1 = int(number1)
+            number2 = int(number2)
+            result = number1 + number2
+            print(f'The result of your sum is {number1} + {number2} = {result}')
 
-    if operator == 'multi':
-        number1 = int(number1)
-        number2 = int(number2)
-        result = number1 * number2
-        print(f'The result of your multiplication is {result}')
-    
-    if operator == 'division':
-        number1 = int(number1)
-        number2 = int(number2)
-        result = number1 // number2
-        print(f'The result of the division is {result}')
+        elif subSelection:
+            number1 = int(number1)
+            number2 = int(number2)
+            result = number1 - number2
+            print(f'The result of your subtraction is {number1} - {number2} = {result}')
 
-    question = input('Would you like to continue to calculate?(Y/N): ')
-    if question == 'Y' or question == 'y':
-        continue
-    elif question == 'N' or question == 'n':
-        print('Thanks for using the calculator!')
-        break
-    else:
-        print('You typed an invalid value')
+        elif multiSelection:
+            number1 = int(number1)
+            number2 = int(number2)
+            result = number1 * number2
+            print(f'The result of your multiplication is {number1} * {number2} = {result}')
+        
+        elif divisionSelection:
+            number1 = int(number1)
+            number2 = int(number2)
+            result = number1 // number2
+            print(f'The result of the division is {number1} / {number2} = {result}')
 
-print('The final result of your operation is', result)
+        question = input('Would you like to continue to calculate?(y/n): ').lower()
+        yesQuestion = question == 'y'
+        noQuestion = question == 'n'
+        if yesQuestion:
+            continue
+        elif noQuestion:
+            break
+        else:
+            print('You typed an invalid value')
+    except:
+        print('You typed an invalid value, try again')
+
+print('Thanks for using the calculator!')
