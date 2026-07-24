e73381f96336adff5ec69a6bef77751119caa51c
jgabriel5th <joaojklgabriel@gmail.com>
2026-07-24 15:30:18 -0300

    Studied more about args

--- python_course/intermediate/lesson7_args.py
+++ python_course/intermediate/lesson7_args.py
@@ -9,13 +9,24 @@ print(x, y, _)
 # def sumNumbers(x, y):
 #     return x + y
 
+# def sumNumbers(*args): # It allows to receive unlimited arguments just like print()
+#     # it creates a tuple in args.
+#     total = 0
+#     for number in args:
+#         print(f'Sum: {total} + {number}')
+#         total += number
+#         print('Total:', total)
+#     return total
+
 def sumNumbers(*args): # It allows to receive unlimited arguments just like print()
     # it creates a tuple in args.
     total = 0
     for number in args:
-        print(f'Sum: {total} + {number}')
         total += number
-        print('Total:', total)
+    return total
 
 
-sumNumbers(1, 2, 3, 4, 5)
+sum_numbers = sumNumbers(1, 2, 3, 4, 5, 6, 7, 8)
+numbers = 1, 2, 3, 4, 5, 6, 7, 8
+print(sumNumbers(*numbers)) # *unpacking in order to avoid creating a tuple inside a tuple in args.
+print(sum(numbers)) # Python has a function that sums numbers.
