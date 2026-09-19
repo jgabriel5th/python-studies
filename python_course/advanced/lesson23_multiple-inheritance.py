caf9327a9a152a7bd1629e77df359fd78e6e46cc
jgabriel5th <joaojklgabriel@gmail.com>
2026-09-19 15:16:06 -0300

    Studied the theory of multiple inheritance in Python

---
+++ python_course/advanced/lesson23_multiple-inheritance.py
@@ -0,0 +1,29 @@
+# Multiple Inheritance - Object-Oriented Python
+# It means that in Python, a class can extend several
+# other classes.
+#
+# Simple Inheritance:
+# Animal -> Mammal -> Human -> Person -> Client
+#
+# Multiple Inheritance and mixes:
+# Log -> FileLog
+# Animal -> Mammal -> Human -> Person -> Client
+# Client(Person, FileLog)
+#
+# A, B, C, D
+# D(B, C) - C(A) - B(A) - A
+#
+# method -> speak
+#           A
+#         /   \
+#        B    C # Diamond Problem
+#        \   /
+#          D
+#
+# Python 3 uses C3 superclass linearization
+# to generate the mro(method resolution order).
+# It's not necessary to be studied(complex)
+#
+# In order to know the method resolution order
+# It can be used the class method Class.mro()
+# Or the attribute __mro__ (Dunder - Double Underscore)
