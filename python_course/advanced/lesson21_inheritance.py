efaf1a83dbe2385b28976bd43ee3cef18d86e74b
jgabriel5th <joaojklgabriel@gmail.com>
2026-09-19 11:01:27 -0300

    Studied the theory of inheritance in OOP

---
+++ python_course/advanced/lesson21_inheritance.py
@@ -0,0 +1,12 @@
+# Simple inheritance - Relationships between classes
+# Association - use
+# Aggregation - has
+# Composition - own
+# Inheritance - It is one
+
+# Inheritance or Composition
+
+# Main class (Person)
+#   -> super class, base class, parent class
+# Children classes (Client)
+#   -> sub class, derived class, child class
